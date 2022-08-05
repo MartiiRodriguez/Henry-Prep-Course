b7ce061f05ddcacf6612043cc863cd32b9abffcc
Martiniano <martinianorodriguez09@gmail.com>
2022-08-05 12:34:24 -0300

    New commit

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -138,24 +138,34 @@ function diaDeLaSemana(numeroDeDia) {
   //Suponga que los días de la semana se codifican como 1 = Domingo, 2 = Lunes y así sucesivamente. 
   //Realiza una función que dado el número del día de la semana, retorne: Es fin de semana
   //si el día corresponde a Sábado o Domingo y “Es dia Laboral” en caso contrario. 
-  //Escribe tu código aquí   
-  
+  //Escribe tu código aquí:
+  if(numeroDeDia === 1 || numeroDeDia === 7){
+    return "Es fin de semana"
+  } else if(numeroDeDia > 1 && numeroDeDia < 7){
+    return "Es dia Laboral"
+  }
 } 
 
 
 function empiezaConNueve(n) {
   //Desarrolle una función que recibe como parámetro un número entero n. Debe retornar true si el entero 
   //inicia con 9 y false en otro caso.
-  //Escribe tu código aquí
-  
+  //Escribe tu código aquí:
+
 }
 
 
 function todosIguales(arreglo) {
   //Escriba la función todosIguales, que indique si todos los elementos de un arreglo son iguales:
   //retornar true, caso contrario retornar false.
-  //Escribe tu código aquí  
-  
+  //Escribe tu código aquí:
+  for(let i = 0; i < arreglo.length; i++){
+    if(arreglo[i] === arreglo[i+1]){
+      return true;
+    }else {
+      return false;
+    }
+  }
 } 
 
 
@@ -164,6 +174,11 @@ function mesesDelAño(array) {
   // "Enero", "Marzo" y "Noviembre", guardarlo en nuevo array y retornarlo.
   //Si alguno de los meses no está, devolver: "No se encontraron los meses pedidos"
   // Tu código:
+  let meses = array.filter((e) => e === "Enero" || e === "Marzo" || e === "Noviembre")
+    if(meses.length < 3) {
+      return "No se encontraron los meses pedidos"
+    }
+   return meses;
 }
 
 
